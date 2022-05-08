1e5f1b6b21ad4b2ab3b41cb89c7f2d297cdfd754
Nikolay Golomolzin <kokamvd@gmail.com>
2022-05-08 23:14:11 +0300

    Add task 3-5 solution

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,6 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,7 +12,26 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Bad request');
+        break;
+      }
 
+      if (!fs.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end('Not found');
+        break;
+      }
+
+      fs.rmSync(filepath);
+      if (!fs.existsSync(filepath)) {
+        res.statusCode = 200;
+        res.end('OK');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal server error');
+      }
       break;
 
     default:
